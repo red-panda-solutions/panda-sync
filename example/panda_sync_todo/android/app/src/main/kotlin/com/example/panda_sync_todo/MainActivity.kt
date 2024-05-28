96fc94bb2ca4ca03ca5002deaf3d1192138ca8a3
AbgarSim <abgar1223@gmail.com>
2024-05-28 20:58:55 +0300

    Removed example application, will be moved to a separate repository

--- example/panda_sync_todo/android/app/src/main/kotlin/com/example/panda_sync_todo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.panda_sync_todo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/pandasyncServerTodo/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tasks (title, priority, status, date) VALUES ('Complete the project documentation', 'High', 1, '2023-05-10T14:00:00');
-INSERT INTO tasks (title, priority, status, date) VALUES ('Buy groceries', 'Low', 0, '2023-05-11T10:00:00');
-INSERT INTO tasks (title, priority, status, date) VALUES ('Schedule annual checkup', 'Medium', 0, '2023-05-12T15:00:00');
-INSERT INTO tasks (title, priority, status, date) VALUES ('Meet with project team', 'High', 1, '2023-05-13T09:30:00');
